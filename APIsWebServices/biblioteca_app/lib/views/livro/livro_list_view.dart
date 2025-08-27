cd2a497a54020f62f9842eb998a290b8ef708fd5
giovana-088 <giovanadasilva200509@gmail.com>
2025-08-27 11:30:06 -0300

    27.08.25

--- APIsWebServices/biblioteca_app/lib/views/livro/livro_list_view.dart
+++ APIsWebServices/biblioteca_app/lib/views/livro/livro_list_view.dart
@@ -1,3 +1,6 @@
+import 'package:biblioteca_app/controllers/livros_controller.dart';
+import 'package:biblioteca_app/models/livros_model.dart';
+import 'package:biblioteca_app/views/livro/livro_form_view.dart';
 import 'package:flutter/material.dart';
 
 class LivroListView extends StatefulWidget {
@@ -8,8 +11,60 @@ class LivroListView extends StatefulWidget {
 }
 
 class _LivroListViewState extends State<LivroListView> {
+  final _controller = LivrosController();
+  List<Livro> _livros = [];
+  bool _carregando = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _carregarDados();
+  }
+
+  Future<void> _carregarDados() async {
+    setState(() {
+      _carregando = true;
+    });
+    try {
+      _livros = await _controller.fetchAll();
+    } catch (e) {
+      // Tratar erro
+      _livros = [];
+    }
+    setState(() {
+      _carregando = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    if (_carregando) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    if (_livros.isEmpty) {
+      return const Center(child: Text('Nenhum livro encontrado.'));
+    }
+    return ListView.builder(
+      itemCount: _livros.length,
+      itemBuilder: (context, index) {
+        final livro = _livros[index];
+        return ListTile(
+          title: Text(livro.titulo),
+          subtitle: Text(livro.autor),
+          trailing: Icon(
+            livro.disponivel ? Icons.check_circle : Icons.cancel,
+            color: livro.disponivel ? Colors.green : Colors.red,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => LivroFormView(livro: livro),
+              ),
+            ).then((_) => _carregarDados());
+          },
+        );
+      },
+    );
   }
 }
